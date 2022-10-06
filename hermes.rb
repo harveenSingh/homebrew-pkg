9f64ff6918b85ca5251c0f4d7587ce37d4c4bb82
Harveen Singh <harveen.s@hotmail.com>
2022-10-06 13:41:12 -0400

    removed default comments

--- hermes.rb
+++ hermes.rb
@@ -1,6 +1,4 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
 class Hermes < Formula
   desc "IBC Relayer in Rust"
   homepage "https://hermes.informal.systems"
@@ -11,20 +9,10 @@ class Hermes < Formula
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cargo", "install", "--locked", "--root", prefix, "--path", "./crates/relayer-cli"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test hermes`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/hermes", "version"
   end
 end
